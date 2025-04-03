a7581a6b6b870bc93b24041433370ca29cb55966
Ananya Rastogi <ananyarastogi.1703@gmail.com>
2025-04-03 23:21:18 +0530

    update

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://ananya013btcsai22:<db_password>@cluster0.7yycwou.mongodb.net/events_app')
+    await mongoose.connect('mongodb+srv://ananya013btcsai22:i8T_BdTYZBhE2Bq@cluster0.7yycwou.mongodb.net/events_app')
     .then(() => {
         console.log("✅ MongoDB connected successfully");
     })
