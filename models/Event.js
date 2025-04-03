f25757f8e8faac15d2d52b5a3b2f9797200be9b2
Ananya Rastogi <ananyarastogi.1703@gmail.com>
2025-04-03 16:16:26 +0530

    update events

---
+++ models/Event.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const eventSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    time: {
+        type: String,
+        required: true,
+    },
+    venue:{
+        type: String,
+        required: true,
+    },
+    genre:{
+        type: String,
+        required: true,
+    },
+    college: {
+        type:String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    registeration_link: {
+        type: String,
+        required: true,
+    },
+    registration_deadline:{
+        type: Date,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const EventModel = mongoose.models.Event || mongoose.model("Event", eventSchema);
+export default EventModel;
